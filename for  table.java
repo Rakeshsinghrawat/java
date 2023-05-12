2481b5868939dca12d114e38611740419ca2e905
rakeshsinghrawat <rakeshsinghrawat82742@gmail.com>
2023-05-12 11:10:12 +0530

    for table

---
+++ for  table.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class Main
+{
+    public static void main(String Arr[])
+    {
+        int n=1,table,r;
+        Scanner ob = new Scanner(System.in);
+        System.out.print("enter table no . ");
+        table= ob.nextInt();
+
+        for(n=1;n<=10;n++);
+        {
+            r= n*table;
+            System.out.println(table+"X"+n+"="+r);
+        }
+        
+    }
+}
