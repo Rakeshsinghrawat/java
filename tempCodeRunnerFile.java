769c5098cb70af40155dfb6ca2a85493e9059c34
rakeshsinghrawat <rakeshsinghrawat82742@gmail.com>
2023-05-15 11:44:43 +0530

    not

--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,19 +1,49 @@
-import java.utill.Scanner;
+import java.util.Scanner;
+class Calculater
 {
-    public static void main(string arr[])
-    {
-        int a;
-        Scanner ob = new Scanner(system.in);
-        system.out.println("enter the no . ");
-        a = nextInt();
-        if(2%a==0)
-        {
-          system.out.println("number is even ");  
-        }
+   public static void main(String arr[])
+   {
+    Scanner ob=new Scanner(System.in);
+    int n1,n2,r;
+    char c;
+    System.out.println("enter 1st no : ");
+    n1=ob.nextInt();
 
-        else
-        {
-          system.out.println("number is odd ");  
-        }
+    System.out.println("enter 2nd no : ");
+    n2=ob.nextInt();
+
+    System.out.println("enter opretor : ");
+    c =ob.next().charAt(0);
+    switch (c) 
+    {
+        case '+':
+          r=n1+n2;
+          System.out.println("sum="+r);  
+        break;
+
+        case '-':
+          r=n1-n2;
+          System.out.println("sub="+r); 
+        break;
+
+        case '*':
+          r=n1*n2;
+          System.out.println("multiply="+r); 
+        break;
+        
+        case '%':
+          r=n1%n2;
+          System.out.println("reminder="+r); 
+        break;
+
+        case '/':
+          r=n1-n2;
+          System.out.println("sub="+r); 
+        break;
+    
+        default:
+        System.out.println("invalid"); 
+        break;
     }
+   }
 }
